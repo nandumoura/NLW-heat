7e8194ae232e7022c93034ec5ca3d19c4bba1369
Fernando Moura <nandumoura@gmail.com>
2021-10-20 17:55:37 -0300

    feat: cria serviço de mensagens e adiciona cors

---
+++ node_heat/prisma/migrations/20211020064626_create_user/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "github_id" INTEGER NOT NULL,
+    "avatar_url" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
--- node_heat/src/app.ts
+++ node_heat/src/app.ts
@@ -1,5 +1,29 @@
+import "dotenv/config";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+import { router } from "./routes";
 
 const app = express();
 
-app.listen(4000, () => console.log("Servidor está no ar cambada"))
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp);
+
+app.use(express.json());
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+  return response.json(code);
+});
+
+app.listen(4000, () => console.log("Servidor está no ar cambada"));
