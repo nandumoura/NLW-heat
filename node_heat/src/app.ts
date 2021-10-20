e9a187fb5f5b19fd6496dd080de19dfad2adce28
Fernando Moura <nandumoura@gmail.com>
2021-10-20 18:19:17 -0300

    feat: add socket io

--- node_heat/src/app.ts
+++ node_heat/src/app.ts
@@ -1,16 +1,26 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import http from "http";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 import { router } from "./routes";
 
 const app = express();
 
+app.use(cors());
+
 const serverHttp = http.createServer(app);
 
-const io = new Server(serverHttp);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
 
+io.on("connection", (socket) => {
+  console.log(`Usuario conectado no socket ${socket.id}`);
+});
 app.use(express.json());
 
 app.use(router);
@@ -26,4 +36,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => console.log("Servidor está no ar cambada"));
+export { serverHttp, io };
